74a41659e122fbcde3780adacc8041322f738e54
giltobias2010 <giltobias2010@gmail.com>
2026-09-07 09:37:43 -0300

    Add files via upload

---
+++ funciones_string.php
@@ -0,0 +1,9 @@
+<?php 
+
+$cadena_texto = "Hola mundo";
+
+$cadena_texto = ucwords($cadena_texto);
+$cadena_texto = strtoupper($cadena_texto);
+$cadena_texto = strtolower($cadena_texto);
+
+echo $cadena_texto;
